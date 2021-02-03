c3b34646a1ab5397193e85ffc4924cd440af84a5
Dale Shoulders <dshoulders@outlook.com>
2021-02-03 17:08:48 +0000

    add some comments on the transform thing

--- react-transformer.js
+++ react-transformer.js
@@ -4,8 +4,10 @@ const { process } = require('babel-jest');
 module.exports = {
     process(src, filename, options) {
 
+        // Swap the local imports for npm packages.
         // jest tests will not work our local version of react/react-dom
-        // swap the local imports for npm packages
+        // because react-testing-library depends on a 'react' npm module
+        // this instance of react must be the same running instance that is rendering components.
         const srcWithReactImportsReplaced = src
             .replace(/from '.*\/react\/react-internal\.js'/mg, 'from \'react\'')
             .replace(/from '.*\/react-dom\/react-dom-internal\.js'/mg, 'from \'react-dom\'')
