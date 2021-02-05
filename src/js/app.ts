f6082dfbcdf69732bab70254d0865ad0e65caa2b
Dale Shoulders <dshoulders@outlook.com>
2021-02-05 20:49:44 +0000

    Add comment to explain use of js extension

--- src/js/app.ts
+++ src/js/app.ts
@@ -3,6 +3,10 @@ import { html } from './utils/markup.js';
 import { ListItemProvider } from './state/listItems.js';
 import ListControls from './components/ListControls.js';
 
+// Import statements should all use .js extension so that they are correct when transpiled
+// Typescript transformations do not alter import paths
+// https://github.com/evanw/esbuild/issues/622 
+
 function App () {
 
     return html`
