b6038adf46802e734364e807b387cbb5ccb31e19
Dale Shoulders <dshoulders@outlook.com>
2021-02-12 21:01:45 +0000

    loading indicator on outcome button

--- src/js/components/Outcome.ts
+++ src/js/components/Outcome.ts
@@ -1,5 +1,5 @@
 import { html } from '../utils/markup.js';
-import { useContext } from '../lib/react/react-internal.js';
+import { useContext, useState } from '../lib/react/react-internal.js';
 import { Component, componentStore, Dispatch as DispatchComponents, ActionType as ActionTypeComponents } from '../context/components.js';
 import { State, stateStore, Dispatch as DispatchState, ActionType as ActionTypeState } from '../context/state.js';
 import { Outcome as TOutcome, outcomeStore, Dispatch as DispatchOutcomes, ActionType as ActionTypeOutcomes } from '../context/outcomes.js';
@@ -8,11 +8,17 @@ import { InvokeType, PLATFROM_URI } from '../constants.js';
 
 const Outcome = ({ outcome }: { outcome: TOutcome }) => {
 
+    const [isLoading, setIsLoading] = useState(false);
+
     const { dispatch: dispatchState, state }: { dispatch: DispatchState, state: State} = useContext(stateStore); 
     const { dispatch: dispatchOutcomes, outcomes }: { dispatch: DispatchOutcomes, outcomes: TOutcome[] } = useContext(outcomeStore);
     const { dispatch: dispatchComponents, components }: { dispatch: DispatchComponents, components: Component[]} = useContext(componentStore);
 
     const onClick = async () => {
+
+        if (isLoading === true) return;
+
+        setIsLoading(true);
 
         const pageComponentInputResponses = components.map(({ id, contentValue, objectData }) => ({
             pageComponentId: id,
@@ -57,10 +63,12 @@ const Outcome = ({ outcome }: { outcome: TOutcome }) => {
             payload: response,
         });
 
+        setIsLoading(false);
+
     };
 
     return html`
-        <button className='outcome' onClick=${onClick}>${outcome.label}</button>
+        <button className='outcome${isLoading ? ' is-loading' : ''}' onClick=${onClick}>${outcome.label}<span className='loader'/></button>
     `;
 }
 
